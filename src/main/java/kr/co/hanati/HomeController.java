fa9a576d291b01d2735cace92f050736ea6355a3
yegyeom yu <meopulior@gmail.com>
2019-01-09 15:43:01 +0900

    카드정보입력 하는중

--- src/main/java/kr/co/hanati/HomeController.java
+++ src/main/java/kr/co/hanati/HomeController.java
@@ -36,4 +36,33 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value = "/newtab", method = RequestMethod.GET)
+	public String newTab(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "newTab";
+	}
+	
+	
+	/* 모바일 카드 등록 */
+	@RequestMapping(value = "/mobile/card/regist", method = RequestMethod.GET)
+	public String mobileCardRegist() {
+		logger.info("카드 등록 화면 요청");
+		return "mobile_card_regist";
+	}
+	
+	/* 모바일 알람 화면 */
+	@RequestMapping(value = "/mobile/push", method = RequestMethod.GET)
+	public String mobilePush() {
+		logger.info("결제 요청 알림을 모바일로 보냄");
+		return "mobile_push";
+	}
+	
 }
